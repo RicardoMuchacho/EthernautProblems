962feaf1a4639bde93bf8370d0696199ea8674c8
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-05-31 20:04:39 +0200

    lvl4 successful attack test

---
+++ Level4/attacker.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {CoinFlip} from "./Fallout.sol";
+
+contract CoinFlipAttack is Script {
+    function run() external {
+        // uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        // vm.startBroadcast(privateKey);
+
+        // Marketplace marketplace = new Marketplace();
+        // vm.stopBroadcast();
+
+        // return marketplace;
+    }
+}
